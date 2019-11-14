9f678a5e6181c6ce12e5beb382091ee8b13abb8c
zhihua.su <zhihua@sg.ibm.com>
2019-11-14 12:12:51 +0800

    Revert "part-2"
    This reverts commit 5770c6c9

--- part-2/depth_rectified_images.m
+++ part-2/depth_rectified_images.m
@@ -4,8 +4,8 @@ function result = depth_rectified_images()
     image2 = double(imread('im6.png'));
     [height, width, ~] = size(image1);
     nodes_count = width * height;
-%     image1 = reshape(image1, 1, nodes_count, 3);
-%     image2 = reshape(image2, 1, nodes_count, 3);
+    image1 = reshape(image1, 1, nodes_count, 3);
+    image2 = reshape(image2, 1, nodes_count, 3);
     
     depth = rgb2gray(im2double(imread('depth.png')));
     [d_height, d_width] = size(depth);
@@ -15,21 +15,14 @@ function result = depth_rectified_images()
     d = 1:80;
     unary = zeros(80, nodes_count);
     for disparity = d
-        displacement = disparity - 40;
-        padding = zeros(height, abs(displacement), 3);
-        if displacement < 0
-            unary(disparity, :) = reshape(sum(abs(cat(2, image2(:, -displacement+1:width, :), padding) - image1), 3) / 3, 1, nodes_count);
-%             unary(disparity, :) = sum(abs(cat(2, image2(1, -displacement+1:nodes_count, :), zeros(1, -displacement, 3)) - image1), 3) / 3;
-        else
-            unary(disparity, :) = reshape(sum(abs(cat(2, padding, image2(:, 1:width - displacement, :)) - image1), 3) / 3, 1, nodes_count);
-%             unary(disparity, :) = sum(abs(cat(2, zeros(1, displacement, 3), image2(1, 1:nodes_count - displacement, :)) - image1), 3) / 3;
-        end
+        displacement = disparity * height;
+        unary(disparity, :) = sum(abs(cat(2, zeros(1, displacement, 3), image2(1, 1:nodes_count - displacement, :)) - image1), 3) / 3;
     end
     
     [~, class] = min(unary); 
     class = class - 1;
     
-    label_cost = single(log(1 + ((d - d').^2) / 2)); % ???
+    label_cost = single(log(1 + ((d - d').^2) / 2));
     
     total_edges = (width - 1) * height + (height - 1) * width;
     i = zeros(total_edges, 1);
@@ -66,10 +59,10 @@ function result = depth_rectified_images()
     min_error = 10000;
     result = uint8(zeros(height, width, 3));
     best_lambda = 0;
-    for lambda = 1:50
+    for lambda = 1:200
         pairwise = sparse(i, j, lambda, nodes_count, nodes_count);
         [label, ~, ~] = GCMex(class, single(unary), pairwise, label_cost, 1);
-        label = reshape(abs(label-40), [height, width]);
+        label = reshape(label, [height, width]);
         label = mat2gray(label);
         error = sum(abs(label - depth), 'all') / nodes_count;
         if error < min_error
