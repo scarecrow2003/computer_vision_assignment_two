7b01c8b9abb85a5bf69f4ba3bfd3d383d807e8de
zhihua.su <zhihua@sg.ibm.com>
2019-11-16 03:16:35 +0800

    commit

--- part-4/initialization.m
+++ part-4/initialization.m
@@ -1,14 +1,14 @@
-function result = initialization()
+function result = initialization1()
     addpath('..');
     addpath('Road');
     addpath(['Road' filesep 'src']);
     image = double(imread('test0000.jpg'));
     [height, width, ~] = size(image);
-    nodes_count = width * height;
+    nodes_count = height * width;
     
-    file = fopen(['Road' filesep 'cameras.txt'], 'r');
+    file = fopen(['Road' filesep 'cameras.txt'],'r');
     total_frame = fscanf(file, '%f', [1, 1]);
-    cameras = fscanf(file, '%f %f %f', [3, Inf]);
+    cameras = fscanf(file, '%f %f %f', [3,Inf]);
     fclose(file);
     
     min_displacement = 0;
@@ -20,29 +20,27 @@ function result = initialization()
     i = zeros(total_edges, 1);
     j = zeros(total_edges, 1);
     current = 1;
-    for y = 1:height
-        for x = 1:width
-            node = (y-1) * width + x;
-            if x < width
-                i(current) = node;
-                j(current) = node + 1;
-                current = current + 1;
-            end
-            if x > 1
-                i(current) = node;
-                j(current) = node - 1;
-                current = current + 1;
-            end
+    for x = 1:width
+        for y = 1:height
+            n_current = (x - 1) * height + y;
             if y < height
-                below = y * width + x;
-                i(current) = node;
-                j(current) = below;
+                i(current) = n_current;
+                j(current) = n_current + 1;
                 current = current + 1;
             end
             if y > 1
-                above = (y-1) * width + x;
-                i(current) = node;
-                j(current) = above;
+                i(current) = n_current;
+                j(current) = n_current - 1;
+                current = current + 1;
+            end
+            if x < width
+                i(current) = n_current;
+                j(current) = n_current + height;
+                current = current + 1;
+            end
+            if x > 1
+                i(current) = n_current;
+                j(current) = n_current - height;
                 current = current + 1;
             end
         end
@@ -60,18 +58,18 @@ function result = initialization()
     n(end, end) = 2;
     n = reshape(n, 1, nodes_count);
     w_s = 5 ./ (max_displacement - min_displacement);
-
+    
     [x, y] = meshgrid(1:displacement, 1:displacement);
     eta = (max_displacement - min_displacement) * 0.05;
     label_cost = single(min(step .* abs(x - y), eta));
-    
+
     [x, y] = meshgrid(1:width, 1:height);
-    location1 = [x(:)'; y(:)'; ones(1, nodes_count)];
+    location1 = [x(:)'; y(:)'; ones(1,nodes_count)];
     sigma_c = 10;
-    
+
     num = 3;
     for n_current = num:140-num
-        image1 = double(imread([['Road' filesep 'src' filesep 'test'], sprintf('%04d', n_current),'.jpg']));
+        image1 = double(imread([['Road' filesep 'src' filesep 'test'], sprintf('%04d', n_current), '.jpg']));
         camera_start = n_current * 7;
         k1 = cameras(:, 1+camera_start:3+camera_start)';
         r1 = cameras(:, 4+camera_start:6+camera_start)';
@@ -79,26 +77,26 @@ function result = initialization()
         
         u_init = zeros(displacement, nodes_count);
         for s = [n_current-3, n_current-2, n_current-1, n_current+1, n_current+2, n_current+3]
-            image2 = double(imread([['Road' filesep 'src' filesep 'test'], sprintf('%04d', s), '.jpg']));
+            image2 = double(imread([['Road' filesep 'src' filesep 'test'], sprintf('%04d',s), '.jpg']));
             camera_start = s * 7;
             k2 = cameras(:, 1+camera_start:3+camera_start)';
             r2 = cameras(:, 4+camera_start:6+camera_start)';
             t2 = cameras(:, 7+camera_start);
 
-            pixel1 = impixel(image1, location1(1,:), location1(2,:));
+            pixel1 = impixel(image1, location1(1, :), location1(2, :));
             u = zeros(displacement, nodes_count);
             for d = 1:displacement
                 location2 = k2 * r2' * r1 / k1 * location1 + k2 * r2' * (t1 - t2) * step * (d - 1);
                 location2 = round(location2 ./ location2(3, :));
                 pixel2 = impixel(image2, location2(1, :), location2(2, :));
                 pixel2(isnan(pixel2)) = 0;
-                u(d, :) = (sigma_c ./ (sigma_c + sqrt(sum((pixel1 - pixel2) .^2, 2))))';
+                u(d, :) = (sigma_c ./ (sigma_c + sqrt(sum((pixel1 - pixel2) .^ 2, 2))))';
             end
             u_init = u_init + u;
         end
         unary = 1 - u_init ./ max(u_init);
         [~, class] = min(unary); 
-        class = class - 1;
+        class = class-1;
         
         image1 = reshape(image1, 1, nodes_count, 3);
         lambda = 1 ./ (sqrt(sum((image1(1, i, :) - image1(1, j, :)) .^ 2, 3)) + epsilon);
@@ -108,9 +106,10 @@ function result = initialization()
         pairwise = sparse(i, j, lambda);
         
         [label, ~, ~] = GCMex(class, single(unary), pairwise, label_cost, 1);
-        label = reshape(label , [height, width]);
+        label = reshape(label, height, width);
         result = mat2gray(label);
         imwrite(result, [['initialization' filesep 'test'], sprintf('%04d', n_current), '.jpg']);
         save([['initialization' filesep 'test'], sprintf('%04d', n_current), '.mat'], 'label');
     end
+
 end
